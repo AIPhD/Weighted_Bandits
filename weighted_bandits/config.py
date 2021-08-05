dea7c85a414e9b90ce553edcbe63e29163526181
AIPhD <sbilaj@gmail.com>
2021-08-05 12:26:57 +0200

    initialize rep

---
+++ weighted_bandits/config.py
@@ -0,0 +1,6 @@
+dimension = 5
+context_size = 200
+beta = 0.1
+lamb = 1
+gamma = 0.1
+epochs = 20000
---
+++ weighted_bandits/run.py
@@ -0,0 +1,10 @@
+import numpy as np
+import training
+
+def main():
+    
+    training.weighted_training()
+
+
+if __name__ == '__main__':
+    main()
